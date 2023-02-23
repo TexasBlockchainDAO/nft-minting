88d522283aef12d2eec4788bd69f92a03b285fa9
kowalski <kowalski@somerset>
2023-02-22 18:34:20 -0600

    small changes

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,9 +1,18 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
+
+import './UserNFT.sol';
 
 contract Factory {
 
-  function openCollection() public returns (address userContract) {
+  mapping (address => mapping (uint256 => address)) userCollection;
+  mapping (address => uint256) numUserCollections;
+
+  function openCollection(string memory name, string memory symbol) external returns (address userContract) {
+    uint256 personalCollectionId = numUserCollections[msg.sender];
+    userContract = address(new UserNFT(name, symbol, msg.sender));
+    userCollection[msg.sender][personalCollectionId] = userContract;
+    numUserCollections[msg.sender]++;
   }
 }
--- contracts/UserNFT.sol
+++ contracts/UserNFT.sol
@@ -1,17 +1,42 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 
-contract UserNFT is ERC721, Ownable {
+contract UserNFT is ERC721URIStorage {
 
-  constructor(string memory name, string memory symbol) 
+  uint256 private nonce;
+  address private owner;
+
+  constructor(string memory name, string memory symbol, address creator) 
   ERC721(name, symbol)
-  {}
-
-  function mint(address account, string calldata tokenUri) public onlyOwner {
-
+  {
+    owner = creator;
   }
+
+  function mint(address account, string calldata tokenUri) public {
+    assembly { // gas savings
+        if iszero(eq(sload(owner.slot), caller())) {
+            revert(0x1c, 0x04)
+        }
+    }
+    _mint(account, totalSupply());
+    _setTokenURI(totalSupply(), tokenUri);
+    nonce++;
+  }
+
+  function totalSupply() internal view returns (uint256) {
+    return nonce;
+  }
+
+  function transferOwnership(address newOwner) external {
+    assembly { // gas savings
+        if iszero(eq(sload(owner.slot), caller())) {
+            revert(0x1c, 0x04)
+        }
+    }
+    owner = newOwner;
+  }
+
 }
