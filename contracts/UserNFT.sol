e36b83d25417b169c01e71496663f3cd21650cc4
kowalski <kowalski@somerset>
2023-02-22 18:55:31 -0600

    working backend

---
+++ contracts/UserNFT.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+
+contract UserNFT is ERC721URIStorage {
+
+  uint256 private nonce;
+  address private owner;
+
+  constructor(string memory name, string memory symbol, address creator) 
+  ERC721(name, symbol)
+  {
+    owner = creator;
+  }
+
+  function mint(address account, string calldata tokenUri) public {
+    assembly { // gas savings
+        if iszero(eq(sload(owner.slot), caller())) {
+            revert(0x1c, 0x04)
+        }
+    }
+    _mint(account, totalSupply());
+    _setTokenURI(totalSupply(), tokenUri);
+    nonce++;
+  }
+
+  function totalSupply() internal view returns (uint256) {
+    return nonce;
+  }
+
+  function transferOwnership(address newOwner) external {
+    assembly { // gas savings
+        if iszero(eq(sload(owner.slot), caller())) {
+            revert(0x1c, 0x04)
+        }
+    }
+    owner = newOwner; // This CAN be address(0) to prevent any further minting!
+  }
+
+}
