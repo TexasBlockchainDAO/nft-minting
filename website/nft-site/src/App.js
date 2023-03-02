6a952fce3c254f0eec5b449a745a86d7b6b1d951
AkshathTaduri <akshathtaduri@gmail.com>
2023-03-01 18:47:03 -0600

    API information added to README.md

--- website/nft-site/src/App.js
+++ website/nft-site/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  function handleClick() {
+    setIsPopupOpen(true);
+  }
+
+  function handleClose() {
+    setIsPopupOpen(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleClick}>Open Pop Up</button>
+      {isPopupOpen && (
+        <div className="popup">
+          <div className="popup-inner">
+            <h2>Pop Up Content Goes Here</h2>
+            <button onClick={handleClose}>Close</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
