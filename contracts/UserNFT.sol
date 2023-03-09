3ef955620fd521173d3fd3b6922274b2c572954c
kowalski <kowalski@somerset>
2023-03-08 19:19:17 -0600

    add tests templates

--- contracts/UserNFT.sol
+++ contracts/UserNFT.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.9;
 
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+import 'hardhat/console.sol';
 
 contract UserNFT is ERC721URIStorage {
 
--- test/usernft.test.js
+++ test/usernft.test.js
@@ -1,1 +1,22 @@
-//
+const { expect } = require("chai");
+let user,user2,owner;
+let userAddress, ownerAddress, user2Address;
+let Token;
+let hardhatToken;
+
+describe("User NFT Contract", function () {
+  beforeEach(async function () {
+    [owner,user,user2] = await ethers.getSigners();
+    Token = await ethers.getContractFactory("UserNFT");
+    this.usernft = await Token.deploy(owner.address, "he", owner.address);
+    userAddress = user.address;
+    user2Address = user2.address;
+    ownerAddress = owner.address;
+  });
+  describe("ERC721 Functionality", function() {
+    it("mints an nft only if user is the owner", async function () {
+      await this.usernft.connect(owner).mint(userAddress, "he");
+      expect(await this.usernft.ownerOf(0)).to.be.eq(user.address);
+    });
+  });
+});
